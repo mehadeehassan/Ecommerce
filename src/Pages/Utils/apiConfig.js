026c1b1a9f3d5ee3db4c486fcff87e8cfec53675
Mehadee Hassan <mehedi19999@gmail.com>
2026-07-02 14:53:17 +0600

    Update backend API URL

--- src/Pages/Utils/apiConfig.js
+++ src/Pages/Utils/apiConfig.js
@@ -1,4 +1,4 @@
-const DEFAULT_API_URL = "https://react-backend-62sn.onrender.com";
+const DEFAULT_API_URL = "https://react-backend-1-2e5t.onrender.comgit add .";
 
 export const API_BASE_URL = (
   import.meta.env.VITE_API_URL || DEFAULT_API_URL
