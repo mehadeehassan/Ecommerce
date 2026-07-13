6cb99d466611919821d0d9f6ed1f98030ba5f580
Mehadee Hassan <mehedi19999@gmail.com>
2026-07-14 04:12:31 +0600

    refactor: update Hero component layout and styling for improved responsiveness

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -55,6 +55,48 @@ const Hero = () => {
     if (!loading && slides.length === 0) return null;
 
     return (
+        // <div className="relative overflow-hidden min-h-138 sm:min-h-163 bg-gray-100 flex justify-center items-center duration-200" >
+        //     {/* background pattern */}
+        //     <div className="h-175 w-175 bg-orange-200 absolute -top-1/2 right-0 rounded-3xl rotate-45 -z[8]">
+        //     </div>
+
+        //     {/* hero section */}
+        //     <div className="container mx-auto pb-8 sm:pb-0">
+        //         <Slider {...settings}>
+        //             {slides.map((slide) => (
+        //                 <div key={slide.id}>
+        //                     <div className="grid grid-cols-1 sm:grid-cols-2">
+        //                         <div className="flex flex-col justify-center gap-4 pt-12 sm:pt-0 text-center sm:text-left order-2 sm:order-1 relative z-10">
+        //                             <h1 className="text-5xl sm:text-6xl lg:text-7xl font-bold">
+        //                                 {slide.title}
+        //                             </h1>
+        //                             <p className="text-sm">
+        //                                 {slide.description}
+        //                             </p>
+        //                             <div data-aos="fade-up" data-aos-duration="500" data-aos-delay="300">
+        //                                 <Link to={getSlideLink(slide)}>
+        //                                     <button className="bg-linear-to-r bg-orange-400 hover:scale-105 duration-200 text-white py-2 px-4 rounded-full">
+        //                                         {slide.button_text}
+        //                                     </button>
+        //                                 </Link>
+        //                             </div>
+        //                         </div>
+        //                         <div className="order-1 sm:order-2">
+        //                             <div className="relative z-10">
+        //                                 <img
+        //                                     src={getImageUrl(slide.image)}
+        //                                     alt={slide.title}
+        //                                     className="w-75 h-75 sm:h-112 sm:w-123 sm:scale-105 lg:scale-120 object-contain mx-auto"
+        //                                 />
+        //                             </div>
+        //                         </div>
+        //                     </div>
+        //                 </div>
+        //             ))}
+        //         </Slider>
+        //     </div>
+        // </div>
+
         <div className="relative overflow-hidden min-h-138 sm:min-h-163 bg-gray-100 flex justify-center items-center duration-200" >
             {/* background pattern */}
             <div className="h-175 w-175 bg-orange-200 absolute -top-1/2 right-0 rounded-3xl rotate-45 -z[8]">
@@ -67,7 +109,7 @@ const Hero = () => {
                         <div key={slide.id}>
                             <div className="grid grid-cols-1 sm:grid-cols-2">
                                 <div className="flex flex-col justify-center gap-4 pt-12 sm:pt-0 text-center sm:text-left order-2 sm:order-1 relative z-10">
-                                    <h1 className="text-5xl sm:text-6xl lg:text-7xl font-bold">
+                                    <h1 className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold">
                                         {slide.title}
                                     </h1>
                                     <p className="text-sm">
@@ -82,11 +124,11 @@ const Hero = () => {
                                     </div>
                                 </div>
                                 <div className="order-1 sm:order-2">
-                                    <div className="relative z-10">
+                                    <div className="relative z-10 flex justify-center">
                                         <img
                                             src={getImageUrl(slide.image)}
                                             alt={slide.title}
-                                            className="w-75 h-75 sm:h-112 sm:w-123 sm:scale-105 lg:scale-120 object-contain mx-auto"
+                                            className="w-48 h-48 xs:w-56 xs:h-56 sm:w-64 sm:h-64 md:w-80 md:h-80 lg:w-96 lg:h-96 xl:w-104 xl:h-104 object-contain mx-auto"
                                         />
                                     </div>
                                 </div>
